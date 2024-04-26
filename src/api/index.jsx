d5510152e73cd529cc1bd8a208a664af9d5139c2
Murali R <muralideva443@gmail.com>
2024-04-26 12:34:32 +0530

    completed

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://backend-v9sb.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-task-management-6ngj.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 export default api;
